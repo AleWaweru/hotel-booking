a7406a7f51806b67b6e94d9e7b1d54ce688191f0
AleWaweru <ngashalex8@gmail.com>
2024-02-28 13:46:40 +0300

    Added search functionality and filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/layout/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import Container from "@/components/Container";
 import { Toaster } from "@/components/ui/toaster"
+import LocationFilter from "@/components/LocationFilter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           >
             <main className="flex flex-col min-h-screen bg-secondary">
               <Navbar />
+              <LocationFilter/>
               <section className="flex-grow">
                 <Container>{children}</Container>
               </section>
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -2,12 +2,49 @@
 
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
+import qs from "query-string";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebounceValue } from "@/hooks/useDebounceValue";
 
 const SearchInput = () => {
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const [value, setValue] = useState(title || "");
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const debounceValue = useDebounceValue<string>(value);
+
+  useEffect(() => {
+    const query = {
+      title: debounceValue,
+    };
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+    router.push(url);
+  }, [debounceValue, router]);
+
+  const OnChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  if (pathname !== "/") return null;
+
   return (
     <div className="relative sm:block hidden">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="Search" className="pl-10 bg-primary/10" />
+      <Input
+        value={value}
+        onChange={OnChange}
+        placeholder="Search"
+        className="pl-10 bg-primary/10"
+      />
     </div>
   );
 };
